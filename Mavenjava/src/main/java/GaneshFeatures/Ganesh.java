24768f602860dff18fcf4864e180d902f08a4fb8
Ganesh Appu <ganesh.fugenx@gmail.com>
2023-05-07 23:09:33 +0530

    ganesh Appu committed

---
+++ Mavenjava/src/main/java/GaneshFeatures/Ganesh.java
@@ -0,0 +1,15 @@
+package GaneshFeatures;
+
+public class Ganesh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi");
+		System.out.println("HiHI");
+		System.out.println("HiHIHI");
+		
+
+	}
+
+}
